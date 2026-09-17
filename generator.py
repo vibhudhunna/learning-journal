dc727dae94884445c6b23912d3078d2745527156
Vibhu <vibhudhunna7@gmail.com>
2026-09-17 23:00:13 +0530

    Added while loop and break statement for input validation

--- generator.py
+++ generator.py
@@ -1,10 +1,13 @@
 import random
 print("--- Secure Password Generator ---")
-user_input = input("Enter desired password length: ")
-try:
-    length = int(user_input)
-    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*"
-    password = "".join(random.sample(chars, length))
-    print("Generated Password: " + password)
-except ValueError:
-    print("Error: That is not a valid number. Please use digits")
+while True:
+    user_input = input("Enter desired password length: ")
+    try:
+        length = int(user_input)
+        break
+    except ValueError:
+        print("Error: That is not a valid number. Please use digits")
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*"
+password = "".join(random.sample(chars, length))
+print("Generated Password: " + password)
