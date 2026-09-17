1fc3b8097395d3c07cd30b5aa28454b8f37bbb7d
Vibhu <vibhudhunna7@gmail.com>
2026-09-17 22:48:05 +0530

    Added try-except block to handle invalid user inpu

--- generator.py
+++ generator.py
@@ -1,8 +1,10 @@
 import random
 print("--- Secure Password Generator ---")
 user_input = input("Enter desired password length: ")
-length = int(user_input)
-
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*"
-password = "".join(random.sample(chars, length))
-print("Generated Password: " + password)
+try:
+    length = int(user_input)
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*"
+    password = "".join(random.sample(chars, length))
+    print("Generated Password: " + password)
+except ValueError:
+    print("Error: That is not a valid number. Please use digits")
