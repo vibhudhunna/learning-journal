04fadce3bb1563aa7beadb2a3210cf085521c562
Vibhu <vibhudhunna7@gmail.com>
2026-09-17 22:34:32 +0530

    Added user input for custom password length

--- generator.py
+++ generator.py
@@ -1,5 +1,8 @@
 import random
 print("--- Secure Password Generator ---")
+user_input = input("Enter desired password length: ")
+length = int(user_input)
+
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*"
-password = "".join(random.sample(chars, 16))
+password = "".join(random.sample(chars, length))
 print("Generated Password: " + password)
