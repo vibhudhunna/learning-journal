7075b9c82b3a94d726e640d5151be73699b52a6e
Vibhu <vibhudhunna7@gmail.com>
2026-09-18 11:16:34 +0530

    Refracted code to use a generate_password function

--- generator.py
+++ generator.py
@@ -1,13 +1,16 @@
 import random
+def generate_password(length):
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*"
+    password = "".join(random.choices(chars, k=length))
+    return password
 print("--- Secure Password Generator ---")
 while True:
     user_input = input("Enter desired password length: ")
+
     try:
         length = int(user_input)
         break
     except ValueError:
-        print("Error: That is not a valid number. Please use digits")
-
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*"
-password = "".join(random.choices(chars, k=length))
-print("Generated Password: " + password)
+        print("Error: That is not a valid number. Please try again. \n")
+final_password = generate_password(length)
+print("Generated Password: "+ final_password)
